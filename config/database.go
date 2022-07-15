8d56adfd1d2e4aeea8520291061232df4b742d50
Rafi Septian Hadi <rafiseptian90@gmail.com>
2022-07-15 15:33:03 +0700

    Init database connection

--- config/database.go
+++ config/database.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func DBConnection() (db *gorm.DB) {
+	dsn := "host=" + os.Getenv("DB_HOST") + " " +
+		"user=" + os.Getenv("DB_USERNAME") + " " +
+		"password=" + os.Getenv("DB_PASSWORD") + " " +
+		"dbname=" + os.Getenv("DB_NAME") + " " +
+		"port=" + os.Getenv("DB_PORT") + " " +
+		"sslmode=disable TimeZone=" + os.Getenv("APP_TIMEZONE")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf(err.Error())
+		return
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/rafiseptian90/GoArticle/config"
+	"log"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DB := config.DBConnection()
+
+	fmt.Println(DB)
 }
